f1667ea589199bcb091ae1bd0e231dafaab6aa47
iLynette <lynetteacholah@icloud.com>
2022-06-09 14:37:08 +0300

    Add tests for users in index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+       context "when the page is opened" do
+        it "return a correct response" do
+          get '/users'
+          expect(response).to have_http_status(200)
+          expect(response.body).to render_template(:index)
+          expect(response.body).to include('Show all users')
+        end
+      end
   end
 end
