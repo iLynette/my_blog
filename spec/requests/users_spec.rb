368c19e96492d12e1cb5c9d085e1352bc5690715
iLynette <lynetteacholah@icloud.com>
2022-06-09 14:52:17 +0300

    Fix linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-       context "when the page is opened" do
-        it "return a correct response" do
-          get '/users'
-          expect(response).to have_http_status(200)
-          expect(response.body).to render_template(:index)
-          expect(response.body).to include('Show all users')
-        end
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    context 'when the page is opened' do
+      it 'return a correct response' do
+        get '/users'
+        expect(response).to have_http_status(200)
+        expect(response.body).to render_template(:index)
+        expect(response.body).to include('Show all users')
       end
+    end
   end
 
   describe 'GET /show' do
-    context "when the page is opened" do
+    context 'when the page is opened' do
       it 'returns the correct response' do
         get '/users/1'
         expect(response).to have_http_status(200)
