0ebb6e19f6ad89256f1fcacdb2c8d698e0381098
iLynette <lynetteacholah@icloud.com>
2022-06-10 07:10:41 +0300

    Add user data in the table

--- db/migrate/20220609185551_create_user.rb
+++ db/migrate/20220609185551_create_user.rb
@@ -1,7 +1,10 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :Photo
+      t.string :Bio
+      t.bigint :PostsCounter
       t.timestamps
     end
   end
