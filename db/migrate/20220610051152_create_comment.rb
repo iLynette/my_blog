9adb2411911015733b75483231916c6e0a3e1b01
iLynette <lynetteacholah@icloud.com>
2022-06-10 11:50:38 +0300

    Add comment data and referece user and post table

--- db/migrate/20220610051152_create_comment.rb
+++ db/migrate/20220610051152_create_comment.rb
@@ -1,7 +1,9 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
+      t.references :Author, index: true, foreign_key {to_table: :user}
+      t.references :Post, index: true, foreign_key {to_table: :post}
+      t.text :Text
       t.timestamps
     end
   end
