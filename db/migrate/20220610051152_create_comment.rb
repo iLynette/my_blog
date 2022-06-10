97e957c8ab7ff3d032ec788aac927bd7401e1d51
iLynette <lynetteacholah@icloud.com>
2022-06-10 15:28:50 +0300

    Fix errors with the tables

--- db/migrate/20220610051152_create_comment.rb
+++ db/migrate/20220610051152_create_comment.rb
@@ -1,8 +1,8 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :Author, index: true, foreign_key {to_table: :user}
-      t.references :Post, index: true, foreign_key {to_table: :post}
+      t.references :Author, index: true, foreign_key: {to_table: :users}
+      t.references :Post, index: true, foreign_key: {to_table: :posts}
       t.text :Text
       t.timestamps
     end
