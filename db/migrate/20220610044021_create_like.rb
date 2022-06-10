0e8d8358f664eb06d9622248f2cc5f445c4ab7f0
iLynette <lynetteacholah@icloud.com>
2022-06-10 08:05:45 +0300

    Reference post and user table in likes table

--- db/migrate/20220610044021_create_like.rb
+++ db/migrate/20220610044021_create_like.rb
@@ -1,7 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-
+      t.references :Author, index: true, foreign_key: {to_table: :user}
+      t.references :Post, index: true, foreign_key: {to_table: :post}
       t.timestamps
     end
   end
