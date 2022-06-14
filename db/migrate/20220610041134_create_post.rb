c62ecb2b26c67f89b401456530ec3ad2a9bc5e62
iLynette <lynetteacholah@icloud.com>
2022-06-14 13:11:52 +0300

    Edit create_post file to match naming convention

--- db/migrate/20220610041134_create_post.rb
+++ db/migrate/20220610041134_create_post.rb
@@ -1,11 +1,11 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :Author, index: true, foreign_key: {to_table: :users}
-      t.string :Title
-      t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.references :author, index: true, foreign_key: {to_table: :users}
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
       t.timestamps
     end
   end
