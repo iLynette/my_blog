6865098fbb3fb5791bed0a6f7df6f47d34350ca6
iLynette <lynetteacholah@icloud.com>
2022-06-14 10:56:33 +0300

    Update comments counter.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
     after_save :update_comments_counter
+
+    def update_comments_counter
+        post.increment!(:comments_counter)
+    end
 end
