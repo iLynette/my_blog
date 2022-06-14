c70234c554d4356a1ca95fb28e091288a14fe499
iLynette <lynetteacholah@icloud.com>
2022-06-14 13:11:36 +0300

    Edit create_like file to match naming convention

--- db/migrate/20220610044021_create_like.rb
+++ db/migrate/20220610044021_create_like.rb
@@ -1,8 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :Author, index: true, foreign_key: {to_table: :users}
-      t.references :Post, index: true, foreign_key: {to_table: :posts}
+      t.references :author, index: true, foreign_key: {to_table: :users}
+      t.references :post, index: true, foreign_key: {to_table: :posts}
       t.timestamps
     end
   end
