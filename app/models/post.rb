4505b708a4f89d58ab70dc2c6e61ab1d6a794227
iLynette <lynetteacholah@icloud.com>
2022-06-14 06:48:01 +0300

    Return 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,8 @@ class Post < ApplicationRecord
     def update_post_counter
         author.increment!(:posts_counter)
     end
+
+    def recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
 end
