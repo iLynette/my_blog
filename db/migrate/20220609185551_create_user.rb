29f08fab948e62d4a958f67a301ef05134056319
iLynette <lynetteacholah@icloud.com>
2022-06-14 13:12:05 +0300

    Edit create_user file to match naming convention

--- db/migrate/20220609185551_create_user.rb
+++ db/migrate/20220609185551_create_user.rb
@@ -2,9 +2,9 @@ class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :Photo
-      t.string :Bio
-      t.bigint :PostsCounter
+      t.string :photo
+      t.string :bio
+      t.bigint :posts_counter
       t.timestamps
     end
   end
