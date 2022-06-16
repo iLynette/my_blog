6f6e8d65ddafeecd817e877c3a22ebf8f3560643
iLynette <lynetteacholah@icloud.com>
2022-06-16 20:22:16 +0300

    Actions for adding new records in db in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
   root 'users#index'
 end
