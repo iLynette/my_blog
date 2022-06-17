eb0248e48b159b99210049f61f436b80beb4ddbc
iLynette <lynetteacholah@icloud.com>
2022-06-17 15:00:31 +0300

    Define create in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,23 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @user = current_user
   end
+
+  def create
+    user = current_user
+    post = current_post
+    comment = Comment.new(comment_params)
+    comment.author = user
+    comment.post = post
+    if comment.save
+      redirect_to user_post_url(id: post.id)
+    else
+      redirect_to new_user_post_comment_url(post_id: post.id, user_id: user.id)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
