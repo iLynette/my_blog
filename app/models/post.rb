af5919066df28bb43bc0c98fddfe1e598bc6d470
iLynette <lynetteacholah@icloud.com>
2022-06-17 20:22:47 +0300

    Add only integer

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,9 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   validates :title, presence: true, length: { minimum: 5, maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: true, only_integer: true,  numericality: { greater_than_r_equal_to: 0 }
+  validates :likes_counter, presence: true, only_integer: true, numericality: { greater_than_or_equal_to: 0 }
+
 
   def update_post_counter
     author.increment!(:posts_counter)
