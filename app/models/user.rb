765ac3382d8169d95e236fbb2496fc881c91aec5
iLynette <lynetteacholah@icloud.com>
2022-06-17 20:12:28 +0300

    Add validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
