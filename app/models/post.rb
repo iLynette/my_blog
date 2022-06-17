4c637d2eceb7f4744efeeb8b93634e08af108c28
iLynette <lynetteacholah@icloud.com>
2022-06-17 21:52:54 +0300

    Fix error with only integer

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   validates :title, presence: true, length: { minimum: 5, maximum: 250 }
-  validates :comments_counter, presence: true, only_integer: true,  numericality: { greater_than_r_equal_to: 0 }
-  validates :likes_counter, presence: true, only_integer: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_r_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 
   def update_post_counter
