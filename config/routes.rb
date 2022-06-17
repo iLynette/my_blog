bbcae77012ee3c2cc4113b9e6c0ed10fba38cff4
iLynette <lynetteacholah@icloud.com>
2022-06-17 14:58:54 +0300

    Change order in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :create, :new, :show] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:new, :create]
     end
