0b96e5f613effcbbfcd6ce2487f2b8524e78dfbf
iLynette <lynetteacholah@icloud.com>
2022-06-17 20:14:47 +0300

    Validate title in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
   after_save :update_post_counter
 
+  validates :title, presence: true
+
   def update_post_counter
     author.increment!(:posts_counter)
   end
