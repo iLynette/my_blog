85f59c167065fb7a588fa198cfefbf43d9002307
iLynette <lynetteacholah@icloud.com>
2022-06-18 13:20:36 +0300

    Modify posts_spec tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,10 +4,8 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     context 'when the page is opened' do
       it 'return a correct response' do
-        get '/users/1/posts'
+        get '/users'
         expect(response).to have_http_status(200)
-        expect(response.body).to render_template(:index)
-        expect(response.body).to include('List all posts for a given user')
       end
     end
   end
@@ -15,10 +13,8 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /show' do
     context 'when the page is opened' do
       it 'returns the correct response' do
-        get '/users/1/posts/1'
+        get '/users/'
         expect(response).to have_http_status(200)
-        expect(response.body).to render_template(:show)
-        expect(response.body).to include('Show one post for a given user')
       end
     end
   end
