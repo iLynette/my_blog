49163264f7e25e9e37658fb76a0ea9e5fbbae9f5
iLynette <lynetteacholah@icloud.com>
2022-06-18 10:31:34 +0300

    Fix typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   validates :title, presence: true, length: { minimum: 5, maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_r_equal_to: 0 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 
