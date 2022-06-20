c4739741dc1bf44a054e93f00425f9a55b503f55
iLynette <lynetteacholah@icloud.com>
2022-06-20 20:24:35 +0300

    Include email and run migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 
   def all_users
     User.all
--- db/migrate/20220620152616_add_name_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :surname, :string
-  end
-end
