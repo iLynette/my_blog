49626c813bce697dacc983eff04ef7cb8965475b
iLynette <lynetteacholah@icloud.com>
2022-06-21 12:05:52 +0300

    Fix error with update_allowed_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,5 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email,:name, :password, :current_password)}
   end
 end
