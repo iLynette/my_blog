fcea6a1c950f9b6b3680c7c0d2e1d01bebd41ab7
iLynette <lynetteacholah@icloud.com>
2022-06-21 11:41:50 +0300

    Add authentication in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
 
   def all_users
     User.all
@@ -7,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def current_post
     User.find(params[:user_id]).posts.find(params[:id] || params[:post_id])
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email,:name, :password, :current_password)}
   end
 end
