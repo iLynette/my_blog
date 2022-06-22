60773e2ef8909f63aa07ebea7144481b471d98d0
iLynette <lynetteacholah@icloud.com>
2022-06-22 23:00:11 +0300

    Add destroy in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :create, :new, :show, :destroy] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
