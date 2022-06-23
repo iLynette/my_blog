0203d161c6b9d37ac19748443fc6357fad50802e
iLynette <lynetteacholah@icloud.com>
2022-06-23 12:30:34 +0300

    Test when a user signs in successfully

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -13,6 +13,26 @@ RSpec.feature 'Tests login page', type: :feature do
         click_button 'Log in'
         expect(page).to have_content 'Invalid Email or password.'
       end
-
+  
+      scenario 'when credentials are wrong' do
+        within 'form' do
+          fill_in 'Email', with: 'gmail.com'
+          fill_in 'Password', with: 'secret'
+        end
+        click_button 'Log in'
+        expect(page).to have_content 'Invalid Email or password.'
+      end
+  
+      scenario 'if credentials are right' do
+        @user1 = User.create!(name: 'Lynette', photo: 'mines', bio: 'Developer.', email: 'lynette@gmail.com',
+                             password: 'lynette123', confirmed_at: Time.now, role: 'admin')
+  
+        within 'form' do
+          fill_in 'Email', with: @user1.email
+          fill_in 'Password', with: @user1.password
+        end
+        click_button 'Log in'
+        expect(page).to have_content 'Signed in successfully'
+      end
     end
   end
