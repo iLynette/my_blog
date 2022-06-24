a63732398713dc4a2bf218bba81d8a71b73c23db
iLynette <lynetteacholah@icloud.com>
2022-06-24 19:57:20 +0300

    Set posts controller class

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,11 @@
+class Api::PostsController < ApplicationController
+    def index
+      @user = current_user
+      @posts = @user.posts.includes(:comments)
+      render json:@posts
+    end
+  
+    def show
+    render json: @posts
+    end
+  end
