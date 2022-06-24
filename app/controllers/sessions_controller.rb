5497d6da6eacb8ce568c5852590029c574d5f034
iLynette <lynetteacholah@icloud.com>
2022-06-25 01:00:47 +0300

    Add response to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,10 @@
 class SessionsController < Devise::SessionsController
     respond_to :json
+    private
+  def respond_with(resource, _opts = {})
+      render json: resource
+    end
+    def respond_to_on_destroy
+      head :ok
+    end
   end
