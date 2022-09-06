8faee3de0248bc929af274f05c891351cc7590f2
iLynette <lynetteacholah@icloud.com>
2022-09-06 22:04:47 +0300

    Fix error with jwt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,11 +308,4 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
-  jwt.dispatch_requests = [
-      ['POST', %r{^/sign_in$}]
-    ]
-    jwt.expiration_time = 1.days.to_i
-  end
 end
